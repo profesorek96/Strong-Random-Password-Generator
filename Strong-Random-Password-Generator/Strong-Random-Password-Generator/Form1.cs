9165f5b181ff8a12bddcd8e926cc5a4d2de747a9
Krzysztof Łos <krzysztof.los@vp.pl>
2017-02-27 01:07:10 +0100

    first commit

---
+++ Strong-Random-Password-Generator/Strong-Random-Password-Generator/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Strong_Random_Password_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
