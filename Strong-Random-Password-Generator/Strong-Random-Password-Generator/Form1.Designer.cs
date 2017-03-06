bdd6b531a8c8ae027a90a5d008ba5f545240692e
Krzysztof Łos <krzysztof.los@vp.pl>
2017-03-06 19:02:57 +0100

    add checkboxy and progrming layout

--- Strong-Random-Password-Generator/Strong-Random-Password-Generator/Form1.Designer.cs
+++ Strong-Random-Password-Generator/Strong-Random-Password-Generator/Form1.Designer.cs
@@ -28,21 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tytul = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.infodlugosc = new System.Windows.Forms.Label();
+            this.duzelit = new System.Windows.Forms.CheckBox();
+            this.malelitery = new System.Windows.Forms.CheckBox();
+            this.cyfry = new System.Windows.Forms.CheckBox();
+            this.znakispecjal = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(225, 142);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(160, 22);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GENERUJ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tytul
+            // 
+            this.tytul.AutoSize = true;
+            this.tytul.Font = new System.Drawing.Font("Segoe UI Emoji", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tytul.Location = new System.Drawing.Point(207, 9);
+            this.tytul.Name = "tytul";
+            this.tytul.Size = new System.Drawing.Size(198, 32);
+            this.tytul.TabIndex = 2;
+            this.tytul.Text = "Generator hasła";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 254);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(565, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // infodlugosc
+            // 
+            this.infodlugosc.AutoSize = true;
+            this.infodlugosc.Location = new System.Drawing.Point(12, 148);
+            this.infodlugosc.Name = "infodlugosc";
+            this.infodlugosc.Size = new System.Drawing.Size(136, 16);
+            this.infodlugosc.TabIndex = 4;
+            this.infodlugosc.Text = "Długość hasła (8-50)";
+            // 
+            // duzelit
+            // 
+            this.duzelit.AutoSize = true;
+            this.duzelit.Location = new System.Drawing.Point(225, 67);
+            this.duzelit.Name = "duzelit";
+            this.duzelit.Size = new System.Drawing.Size(53, 20);
+            this.duzelit.TabIndex = 5;
+            this.duzelit.Text = "A...Z";
+            this.duzelit.UseVisualStyleBackColor = true;
+            // 
+            // malelitery
+            // 
+            this.malelitery.AutoSize = true;
+            this.malelitery.Location = new System.Drawing.Point(335, 67);
+            this.malelitery.Name = "malelitery";
+            this.malelitery.Size = new System.Drawing.Size(50, 20);
+            this.malelitery.TabIndex = 6;
+            this.malelitery.Text = "a...z";
+            this.malelitery.UseVisualStyleBackColor = true;
+            // 
+            // cyfry
+            // 
+            this.cyfry.AutoSize = true;
+            this.cyfry.Location = new System.Drawing.Point(225, 102);
+            this.cyfry.Name = "cyfry";
+            this.cyfry.Size = new System.Drawing.Size(50, 20);
+            this.cyfry.TabIndex = 7;
+            this.cyfry.Text = "0...9";
+            this.cyfry.UseVisualStyleBackColor = true;
+            // 
+            // znakispecjal
+            // 
+            this.znakispecjal.AutoSize = true;
+            this.znakispecjal.Location = new System.Drawing.Point(335, 102);
+            this.znakispecjal.Name = "znakispecjal";
+            this.znakispecjal.Size = new System.Drawing.Size(60, 20);
+            this.znakispecjal.TabIndex = 8;
+            this.znakispecjal.Text = "!@#...";
+            this.znakispecjal.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 316);
+            this.ClientSize = new System.Drawing.Size(609, 316);
+            this.Controls.Add(this.znakispecjal);
+            this.Controls.Add(this.cyfry);
+            this.Controls.Add(this.malelitery);
+            this.Controls.Add(this.duzelit);
+            this.Controls.Add(this.infodlugosc);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tytul);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Strong Random Password Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label tytul;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label infodlugosc;
+        private System.Windows.Forms.CheckBox duzelit;
+        private System.Windows.Forms.CheckBox malelitery;
+        private System.Windows.Forms.CheckBox cyfry;
+        private System.Windows.Forms.CheckBox znakispecjal;
     }
 }
 
--- Strong-Random-Password-Generator/Strong-Random-Password-Generator/Form1.cs
+++ Strong-Random-Password-Generator/Strong-Random-Password-Generator/Form1.cs
@@ -16,5 +16,54 @@ namespace Strong_Random_Password_Generator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            int dlugosc = Convert.ToInt32(numericUpDown1.Value);
+
+            bool duze = false;
+            bool male = false;
+            bool liczby = false;
+            bool znaki = false;
+
+            //gdy nie jest nic zaznacozne
+
+            if (duzelit.Checked == true) duze = true;
+            if (malelitery.Checked == true) male = true;
+            if (cyfry.Checked == true) liczby = true;
+            if (znakispecjal.Checked == true) znaki = true;
+            //else znaki = false;
+           
+            textBox1.Text = generuj(duze,male,liczby, znaki,dlugosc);
+       
+        }
+
+        public string generuj(bool duze, bool male, bool cyfry, bool znaki, int dlugosc)
+        {
+
+            string haslo="";
+            char[] alfabetd = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'Q', 'P', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z' };
+            char[] alfabetm = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'q', 'p', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z' };
+            char[] liczbytab = { '0','1','2','3','4','5','6','7','8','9'};
+            char[] znakitab = {'`','~','!','@','#','$','%','^','&','*','=','+','?' };
+
+            string zbior_losowania = "";
+            if (duze == true) for(int i=0;i<alfabetd.Length;i++)zbior_losowania+=alfabetd[i];
+            if (male == true) for (int i = 0; i < alfabetm.Length; i++) zbior_losowania += alfabetm[i];
+            if (cyfry == true) for (int i = 0; i < liczbytab.Length; i++) zbior_losowania += liczbytab[i];
+            if (znaki == true) for (int i = 0; i < znakitab.Length; i++) zbior_losowania += znakitab[i];
+
+            int d = zbior_losowania.Length;
+            Random rand = new Random();
+            for (int i = 0; i < dlugosc; i++)
+            {
+                
+                haslo +=  zbior_losowania[rand.Next(d)];
+            }
+            return haslo;
+        }
+
+        
     }
 }
